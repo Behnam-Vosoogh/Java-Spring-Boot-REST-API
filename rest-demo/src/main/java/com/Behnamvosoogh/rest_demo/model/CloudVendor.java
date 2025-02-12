75563b3d11b9fc7fd7543a2c6eca5a79dff09550
Behnam <behnam.vosoogh@outlook.com>
2025-02-12 00:09:06 -0500

    Data Model Class Created

---
+++ rest-demo/src/main/java/com/Behnamvosoogh/rest_demo/model/CloudVendor.java
@@ -0,0 +1,22 @@
+package com.Behnamvosoogh.rest_demo.model;
+
+public class CloudVendor {
+
+	private String vendoreId;
+	private String vendoreName;
+	private String vendoreAddress;
+	private String vendorerPoneNumber;
+	public CloudVendor(String vendoreId, String vendoreName, String vendoreAddress, String vendorerPoneNumber) {
+		super();
+		this.vendoreId = vendoreId;
+		this.vendoreName = vendoreName;
+		this.vendoreAddress = vendoreAddress;
+		this.vendorerPoneNumber = vendorerPoneNumber;
+	}
+	public CloudVendor() {
+		super();
+	}
+
+	
+
+}
